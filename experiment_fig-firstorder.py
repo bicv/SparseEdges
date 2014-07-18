88c3c8eb24ec2dcf71a36fc06f5cc5c6575ef3d9
Laurent Perrinet <laurent.perrinet@gmail.com>
2014-07-18 14:36:53 +0200

    small diff to run prior firstorder

--- experiment_fig-firstorder.py
+++ experiment_fig-firstorder.py
@@ -13,9 +13,10 @@ mp = SparseEdges(lg)
 
 imageslist, edgeslist, RMSE = mp.process(exp='testing_vanilla', name_database='serre07_distractors')
 
-v_hist, v_theta_edges = mp.histedges_theta(edgeslist, display=False)
+if not(imagelist=='locked'):
+    v_hist, v_theta_edges = mp.histedges_theta(edgeslist, display=False)
 
-z = np.linspace(.5/pe.n_theta, 1.-.5/pe.n_theta, pe.n_theta)
-mp.theta = np.interp(z, np.hstack((0, np.cumsum(v_hist))), v_theta_edges)
+    z = np.linspace(.5/pe.n_theta, 1.-.5/pe.n_theta, pe.n_theta)
+    mp.theta = np.interp(z, np.hstack((0, np.cumsum(v_hist))), v_theta_edges)
 
-mp.process('testing_vanilla_firstorder')
+    mp.process('testing_vanilla_firstorder')
