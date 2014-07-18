b7a07a65dc3c3f83a81624ad47d2e636b33a2171
Laurent Perrinet <laurent.perrinet@gmail.com>
2014-07-18 14:38:16 +0200

    making a debug mode

--- default_param.py
+++ default_param.py
@@ -6,8 +6,8 @@
 # Image
 # 'N_X' : 256, # size of images
 # 'N_Y' : 256, # size of images
-'N_X' : 25, # size of images
-'N_Y' : 25, # size of images
+'N_X' : 32, # size of images
+'N_Y' : 32, # size of images
 'do_mask'  : True, # used in MAtching Pursuit self.pe.do_mask
 # Log-Gabor
 #'base_levels' : 2.,
