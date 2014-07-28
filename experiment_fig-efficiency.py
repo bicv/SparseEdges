4b0b363cee15ebbbc5ff9b90fb1b317a4a43e6b3
Laurent Perrinet <laurent.perrinet@gmail.com>
2014-07-28 16:27:55 +0200

    adding an experiment for the efficiency

--- experiment_fig-efficiency.py
+++ experiment_fig-efficiency.py
@@ -5,7 +5,7 @@ from SLIP import Image
 from LogGabor import LogGabor
 from SparseEdges import SparseEdges
 
-
+pe = ParameterSet('default_param.py')
 def init_pe(pe):
     im = Image(pe)
     lg = LogGabor(im)
