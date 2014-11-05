28410418768b3f47464ec9f804a8b1d50a698124
Laurent Perrinet <laurent.perrinet@gmail.com>
2014-11-05 18:05:52 +0100

    fine tuning experiment testing

--- experiment_testing.py
+++ experiment_testing.py
@@ -7,6 +7,7 @@ statistics on that.
 
 rm -fr mat/edges/testing_* mat/testing_* figures/edges/testing_* figures/testing_*
 frioul_batch  -n "14,15,16"  -M 36 'python experiment_testing.py'
+frioul_batch -M 200 'python experiment_testing.py'
 
 """
 __author__ = "(c) Laurent Perrinet INT - CNRS"
@@ -20,6 +21,7 @@ from LogGabor import LogGabor
 from SparseEdges import SparseEdges
 from NeuroTools.parameters import ParameterSet
 pe = ParameterSet('default_param.py')
+FORMATS = ['pdf', 'eps']
 
 def init_pe(pe, N_X=pe.N_X, N_image=pe.N_image, N=pe.N):
     pe.datapath = '../AssoField/database/'
@@ -43,6 +45,7 @@ mp.process('testing_noise', noise=pe.noise)
 mp.process('testing_vanilla', name_database='serre07_targets')
 
 pe = ParameterSet('default_param.py')
+
 # TODO : make an experiment showing that using scale does not bring much
 ##! comparing representation parameters
 ##!------------------------------------
@@ -59,36 +62,32 @@ mp = init_pe(pe_)
 mp.process('testing_long')
 
 ## other candidate parameters from class SparseEdges:
-##    n_levels = 5, n_theta = 16, B_sf_ratio = 3.
-pe_ = ParameterSet('default_param.py')# = pe.copy()
-pe_.n_theta = pe.n_theta*2
-mp = init_pe(pe_)
-mp.process('testing_moretheta')
+##   B_sf_ratio = 3.
 
-# is whitening important?
-pe_ = ParameterSet('default_param.py')
-pe_.do_whitening = False
-mp = init_pe(pe_)
-# mp.process('testing_nowhite')
+import matplotlib.pyplot as plt
+fig_width_pt = 318.670*.61 # Get this from LaTeX using \showthe\columnwidth
+inches_per_pt = 1.0/72.27               # Convert pt to inches
+fig_width = fig_width_pt*inches_per_pt  # width in inches
 
-#! softy MP with a lower alpha value
-pe_ = ParameterSet('default_param.py')
-pe_.MP_alpha = .25
-mp = init_pe(pe_)
-mp.process('testing_smooth')
+mps, experiments = [], []
+v_alpha = np.linspace(0.3, 1., 5)
+for MP_alpha in v_alpha:
+    pe = ParameterSet('default_param.py')
+    pe.MP_alpha = MP_alpha
+    mp = init_pe(pe)
+    exp = 'efficiency_MP_alpha_' + str(MP_alpha).replace('.', '_')
+    mp.process(exp)
+    experiments.append(exp)
+    mps.append(mp)
 
-#! hard MP with a full alpha value
-pe_ = ParameterSet('default_param.py')#
-pe_.MP_alpha = 1.
-mp = init_pe(pe_)
-mp.process('testing_hard')
-
-##! comparing edge methods
-##!-----------------------
-## parameters from class EdgeFactory:
-pe_ = ParameterSet('default_param.py')#
-pe_.N = pe.N*2
-mp = init_pe(pe_)
-mp.process('testing_moreN')
+databases = ['serre07_distractors'] * len(experiments)
+labels = ['%0.2f' % MP_alpha for MP_alpha in v_alpha]
+fig = plt.figure(figsize=(fig_width, fig_width/1.618))
+fig, a, ax = plot(mps=mps,
+                  experiments=experiments, databases=databases, labels=labels, 
+                  fig=fig, color=[0., 1., 0.], threshold=threshold, scale=True)    
+a.set_xlabel(r' $\alpha$')
+for ext in FORMATS: fig.savefig(mp.figpath + 'testing_alpha.' + ext)
+        
 
 ## TODO:  would be interesting to see how that changes with number of image patches used, i.e. whether it settles down to that particular pattern or just jumps around.
