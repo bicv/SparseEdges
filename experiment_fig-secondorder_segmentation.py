e88a418fa6fc695aaa86792097b724c80cd4bfe0
Laurent Perrinet <laurent.perrinet@gmail.com>
2014-12-03 16:31:03 +0100

    secondorder

--- experiment_fig-secondorder_segmentation.py
+++ experiment_fig-secondorder_segmentation.py
@@ -1,3 +1,7 @@
+
+# rm **/Geisler01Fig7A*
+
+
 import __init__
 import numpy as np
 from NeuroTools.parameters import ParameterSet
@@ -13,7 +17,19 @@ pe.N = 64
 pe.n_theta = 48
 pe.do_whitening = True
 
-# defining input image as Lena
+
+figpath = '../../CNRS/BICV-book/BICV_INT/BICV-sparse/'
+FORMATS = ['pdf', 'eps']
+fig_width_pt = 318.670  # Get this from LaTeX using \showthe\columnwidth
+inches_per_pt = 1.0/72.27               # Convert pt to inches
+fig_width = fig_width_pt*inches_per_pt  # width in inches
+pe.figsize_edges = 12
+pe.figsize_edges = .382 * fig_width
+pe.scale = 1.
+pe.line_width = 1.5
+
+
+# defining input image 
 from pylab import imread
 image = imread('database/Geisler01Fig7A.png').mean(axis=-1)
 print image.mean(), image.std()
@@ -37,6 +53,11 @@ except:
     edges, C_res = mp.run_mp(image, verbose=True)
     np.save(matname, edges)    
 
+edges = np.load(matname)
+fig, a = mp.show_edges(edges, image=image, v_min=v_min, v_max=v_max, color='toto', show_phase=False) #
+if not(figpath==None): 
+    for ext in FORMATS: fig.savefig(figpath + 'Geisler01Fig7_A.' + ext)
+        
 print ' with second-order '
 matname = 'mat/Geisler01Fig7A_secondorder.npy'
 try:
@@ -46,8 +67,14 @@ except:
     edges, C_res = mp.run_mp(image, verbose=True)
     np.save(matname, edges)    
 
+edges = np.load(matname)
+edges[4, :] *= -1 # turn red in blue...
+fig, a = mp.show_edges(edges, image=image, v_min=v_min, v_max=v_max, color='toto', show_phase=False) #
+if not(figpath==None): 
+    for ext in FORMATS: fig.savefig(figpath + 'Geisler01Fig7_B.' + ext)
+
 if True:    
-    for mp.pe.eta_SO in np.linspace(.5, 2.5, 9):
+    for mp.pe.eta_SO in np.linspace(.05, 1., 9):
         matname = 'mat/Geisler01Fig7A_secondorder_' + str(mp.pe.eta_SO).replace('.', '_') + '.npy'
         try:
             edges = np.load(matname)
