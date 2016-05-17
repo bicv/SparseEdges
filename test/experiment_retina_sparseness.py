4b23600ce9d0fde4903ce01f397f7b6929c7e4b6
Laurent Perrinet <laurent.perrinet@gmail.com>
2016-05-17 10:23:12 +0200

    Figure~\ref{fig:retina_sparseness}

--- test/experiment_retina_sparseness.py
+++ test/experiment_retina_sparseness.py
@@ -14,7 +14,8 @@ for name_database in ['serre07_distractors']:#, 'serre07_distractors_urban', 'la
     mp = SparseEdges('https://raw.githubusercontent.com/meduz/SparseEdges/master/default_param.py')
     mp.pe.datapath = '../../SLIP/database/'
     mp.pe.N_image = 100
-    mp.pe.N = 2**14
+    mp.pe.do_mask = True
+    mp.pe.N = 2**12
     mp.pe.n_theta = 1
     mp.pe.B_theta = np.inf
     mp.pe.line_width = 0
@@ -23,7 +24,8 @@ for name_database in ['serre07_distractors']:#, 'serre07_distractors_urban', 'la
     imageslist, edgeslist, RMSE = mp.process(exp='retina_sparseness', name_database=name_database)
     mps.append(mp)
     # control experiment
-    mp.pe.MP_alpha = np.inf
-    mp.init()
-    imageslist, edgeslist, RMSE = mp.process(exp='retina_sparseness_linear', name_database=name_database)
-    mps.append(mp)
+    if True:
+        mp.pe.MP_alpha = np.inf
+        mp.init()
+        imageslist, edgeslist, RMSE = mp.process(exp='retina_sparseness_linear', name_database=name_database)
+        mps.append(mp)
